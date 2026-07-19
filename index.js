b601559128eff576dbc1affffa79b2968f7a6b39
murtaza <279697619+murtazamustafa543-jpg@users.noreply.github.com>
2026-07-19 14:38:53 +0500

    Stage 1: root and health endpoints

--- index.js
+++ index.js
@@ -16,9 +16,27 @@ app.get('/',(req,res)=>{
  });
 
 });
+
+app.get('/s1',(req,res)=>{
+ res.json({
+     name: "Task API",
+      version: "1.0", 
+      endpoints: ["/s1","/health"] 
+ });
+
+});
+
+app.get('/health',(req,res)=>{
+res.json({
+ status:"ok"
+});
+});
+
  app.listen(3000,()=>{
    console.log(`Server is running on http://localhost:3000`);
      console.log(`for intro http://localhost:3000/`);
+     console.log(`for stage 1 http://localhost:3000/s1`);
+     console.log(`for health http://localhost:3000/health`);
  })
 
 
